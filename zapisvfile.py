68295d36c20fbd6c27b735445c5cbc6be4e1ac77
As-Ter01d <72524177+As-Ter01d@users.noreply.github.com>
2020-10-07 23:15:38 +0300

    Delete zapisvfile.py

--- zapisvfile.py
+++
@@ -1,8 +0,0 @@
-fp=open("index.html", "w")
-print("<h1>Заголовок</h1>", file=fp)
-fp.close()
-def generate_body(header, paragraphs):
-    body = "<h1>" + header + "</h1>"
-    for p in paragraphs:
-        body +="<p>"+p+"</p>"
-    return "<body>" + body + "</body>"
